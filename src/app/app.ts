c9ab243d604bd14f60a4fec12d75772cf9b43bda
hiram <hiramhdez98@gmail.com>
2026-01-06 02:10:18 -0600

    add: style to header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutMe } from './Home/about-me/about-me';
+import { Header } from './Home/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AboutMe,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
