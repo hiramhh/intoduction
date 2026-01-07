00def0215afe3c3517bb7eb7e7c9afba69fcbfbd
hiram <hiramhdez98@gmail.com>
2026-01-07 15:23:30 -0600

    add: contact and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutMe } from './Home/about-me/about-me';
 import { Header } from './Home/header/header';
+import { Contact } from './Home/contact/contact';
+import { Footer } from './Home/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AboutMe,Header],
+  imports: [RouterOutlet,AboutMe,Header,Contact,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
