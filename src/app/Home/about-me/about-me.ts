72815d9d25e52e61158717593dc1a9cf53e988f0
hiram <hiramhdez98@gmail.com>
2026-02-26 00:14:23 -0600

    add: description-backend-frontend

--- src/app/Home/about-me/about-me.ts
+++ src/app/Home/about-me/about-me.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -19,7 +18,9 @@ export class AboutMe {
     this.positionDeveloper = 'Sofware Engineer';
     this.developerDescription = `I'm a software developer about 2.5+ years of expirence at retail store. I've been working in diffents process related with the creating of loans and credits systems.`;
 
-    this.frontEndDescription = ``
+    this.frontEndDescription = `I have deep knowledge of the Angular enviroment. I have worked on making http requests and displaying data from databases in user interfaces`;
+    this.backEndDescription = `I have experience connecting web services to databases, managing end-data user and performing calculations. Additionally, I have worked with different lenguages such as Java, PHP and Go`;
+    this.dataBaseDescription = `I worked on developing scripts in an SQL environment, managing user data, and configuringg tables and conections with web services`;
   }
 
 }
